8fdd769fc0f74bfaaed96673c8416999263dc0eb
Andrew Mayes <andrewmayes14@hotmail.com>
2024-02-25 17:59:37 +0100

    login info

--- login.py
+++ login.py
@@ -89,12 +89,12 @@ class Login:
                 raise Exception("load cookies from files fatal. Please check the format")
 
     def _request_get(self, url: str, params=None, allow_redirects=True) -> requests.Response:
-        print(url, flush=True)
-        #url = "https://huggingface.co/oauth/authorize?client_id=8f1a1d63-479b-46c8-84cb-521fe9f3222f&scope=openid%20profile&response_type=code&redirect_uri=https%3A%2F%2Fhuggingface.co%2Fchat%2Flogin%2Fcallback&state=eyJkYXRhIjp7ImV4cGlyYXRpb24iOjE3MDg4ODM0MDU4NTksInJlZGlyZWN0VXJsIjoiaHR0cHM6Ly9odWdnaW5nZmFjZS5jby9jaGF0L2xvZ2luL2NhbGxiYWNrIn0sInNpZ25hdHVyZSI6Ijk5NTZhNmU0MDk2ZTg5ZDg2ZGZmMTM3ZjI0NjY0ZTM3MDExNWE3OWNjZTYyMzM0MjEyZGY1NGJiNzE3MjFlNzcifQ%3D%3D"
-        print(params)
-        print(self.headers)
-        print(self.cookies)
-        print(allow_redirects)
+        # print(url, flush=True)
+        # #url = "https://huggingface.co/oauth/authorize?client_id=8f1a1d63-479b-46c8-84cb-521fe9f3222f&scope=openid%20profile&response_type=code&redirect_uri=https%3A%2F%2Fhuggingface.co%2Fchat%2Flogin%2Fcallback&state=eyJkYXRhIjp7ImV4cGlyYXRpb24iOjE3MDg4ODM0MDU4NTksInJlZGlyZWN0VXJsIjoiaHR0cHM6Ly9odWdnaW5nZmFjZS5jby9jaGF0L2xvZ2luL2NhbGxiYWNrIn0sInNpZ25hdHVyZSI6Ijk5NTZhNmU0MDk2ZTg5ZDg2ZGZmMTM3ZjI0NjY0ZTM3MDExNWE3OWNjZTYyMzM0MjEyZGY1NGJiNzE3MjFlNzcifQ%3D%3D"
+        # print(params)
+        # print(self.headers)
+        # print(self.cookies)
+        # print(allow_redirects)
         for i in range(5):
             try:
                 res = requests.get(
@@ -108,6 +108,11 @@ class Login:
                 break 
             except:
                 print("Trying to Login")
+                print(url, flush=True)
+                print(params)
+                print(self.headers)
+                print(self.cookies)
+                print(allow_redirects)
         return res
 
     def _request_post(self, url: str, headers=None, params=None, data=None, stream=False,
