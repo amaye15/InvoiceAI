d22cd308ef2446884bd4a325a723801a5204bb69
Andrew Mayes <andrewmayes14@hotmail.com>
2024-02-25 16:06:46 +0100

    final app update

--- app.py
+++ app.py
@@ -1,50 +1,93 @@
-import streamlit as st
 import os
-from PIL import Image
-import base64
+import json_repair
+
+import streamlit as st
+
+from lib import *
+
+st.set_page_config(layout="wide")
+
+###################################################
+# Section - Constants
+###################################################
 
 # Supporting both images and PDFs
 ALLOWED_IMAGE_TYPES = ["jpeg", "jpg", "png"]
 ALLOWED_FILE_TYPES = ALLOWED_IMAGE_TYPES + ["pdf"]
-
-def get_invoice_files():
-    invoices_dir = './invoices'
-    return [os.path.join(invoices_dir, f) for f in os.listdir(invoices_dir) if any(f.endswith(ext) for ext in ALLOWED_FILE_TYPES)]
+MODEL = "NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO"
+JSON_FORMATTER = 'format.json'
+INVOICE_PATH = './invoices'
 
 st.title('Invoice Viewer')
 
-# Upload an image or PDF
-uploaded_file = st.file_uploader("Choose an image or PDF file...", type=ALLOWED_FILE_TYPES)
+# Create two columns
+col1, col2 = st.columns(2)
 
-# Select an image or PDF from the directory
-if uploaded_file is None:
-    invoice_files = get_invoice_files()
-    selected_file = st.selectbox('Or choose a pre-existing invoice file:', invoice_files, placeholder = "Choose an option")
-else:
-    selected_file = None
+###################################################
+# Section - Select/ Upload File
+###################################################
 
-# Display the uploaded or selected file
-if uploaded_file is not None:
-    # Check file type and display accordingly
-    if any(uploaded_file.name.endswith(ext) for ext in ALLOWED_IMAGE_TYPES):
-        # Display image
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-    elif uploaded_file.name.endswith('pdf'):
-        # Display PDF
-        base64_pdf = base64.b64encode(uploaded_file.read()).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-        st.markdown(pdf_display, unsafe_allow_html=True)
+with col1:
 
-elif selected_file:
-    # Check file type and display accordingly
-    if any(selected_file.endswith(ext) for ext in ALLOWED_IMAGE_TYPES):
-        # Display image
-        image = Image.open(selected_file)
-        st.image(image, caption='Selected Image', use_column_width=True)
-    elif selected_file.endswith('pdf'):
-        # Display PDF
-        with open(selected_file, "rb") as file:
-            base64_pdf = base64.b64encode(file.read()).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-        st.markdown(pdf_display, unsafe_allow_html=True)
+    # Upload an image or PDF
+    uploaded_file = st.file_uploader("Choose an image or PDF file...", type=ALLOWED_FILE_TYPES)
+
+    # Select an image or PDF from the directory
+    if uploaded_file is None:
+        invoice_files = get_invoice_files(INVOICE_PATH, ALLOWED_FILE_TYPES)
+        selected_file = st.selectbox('Or choose a pre-existing invoice file:', invoice_files, format_func=lambda x: os.path.basename(x))
+    else:
+        selected_file = None
+
+    # Display the uploaded or selected file
+    if uploaded_file is not None:
+        file_type = 'image' if any(uploaded_file.name.endswith(ext) for ext in ALLOWED_IMAGE_TYPES) else 'pdf'
+        ocr_extracted_text = extract_ocr_text(None, uploaded_file, file_type)
+    elif selected_file:
+        file_type = 'image' if any(selected_file.endswith(ext) for ext in ALLOWED_IMAGE_TYPES) else 'pdf'
+        ocr_extracted_text = extract_ocr_text(selected_file, None, file_type)
+
+###################################################
+# Section - LLM Application
+###################################################
+
+# Note I have use huggingchat to use huggingface models, but this code could easily be replaced with OpenAI
+    # OpenAI solutions could be to use directly image to text or text to text with text extraction being done before hand
+
+with col2:
+    json_structure = load_data(JSON_FORMATTER)
+
+    # Generate the prompt using the cached function
+    prompt = generate_prompt(ocr_extracted_text, json_structure)
+
+    llm_extracted_text = query_llm(prompt, MODEL)
+
+    json_text = json_repair.repair_json(llm_extracted_text, return_objects=True)
+
+    ###################################################
+    # Section - JSONify
+    ###################################################
+
+    # Load and display the editable DataFrame
+    data = load_data(JSON_FORMATTER)
+
+    df = create_dataframe(json_text)
+    for col in df.columns:
+        df[col] = df[col].astype(str)
+
+    edited_df = st.data_editor(df, key="data_editor", use_container_width=True, height=800)
+
+# Creating side by side buttons for JSON creation and download
+col_left, col_right = st.columns(2)
+
+with col_left:
+    # Button to manually refresh the JSON data
+    if st.button('Create JSON'):
+        data = update_json(data, df, edited_df)
+        st.success('JSON data has been created.')
+        st.json(data)
+
+with col_right:
+    # Download JSON option
+    st.download_button(label="Download JSON", data=json.dumps(data, indent=4, sort_keys=True), file_name="data.json", mime="application/json")
+
---
+++ lib.py
@@ -0,0 +1,128 @@
+import os
+import re
+import json
+import pytesseract
+
+from PIL import Image
+from hugchat import hugchat
+from hugchat.login import Login
+
+import pandas as pd
+import streamlit as st
+
+from pdf2image import convert_from_path, convert_from_bytes
+
+def get_invoice_files(invoices_dir: str, allowed_file_types: list):
+    return [os.path.join(invoices_dir, f) for f in os.listdir(invoices_dir) if any(f.endswith(ext) for ext in allowed_file_types)]
+
+def load_data(file_path: str):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+    
+@st.cache_data(show_spinner = "OCR Text Extraction")
+def extract_ocr_text(file_path: str, file_content = None, file_type: str ='image'):
+    ocr_text = ""
+    if file_type == 'image':
+        image = Image.open(file_path if file_path else file_content)
+        st.image(image, caption='Image', use_column_width=True)
+        ocr_text += "\n" + pytesseract.image_to_string(image)
+    elif file_type == 'pdf':
+        pages = convert_from_path(file_path) if file_path else convert_from_bytes(file_content)
+        for page in pages:
+            st.image(page, caption='Uploaded Image', use_column_width=True)
+            ocr_text += "\n" + pytesseract.image_to_string(page)
+    return ocr_text
+    
+
+@st.cache_data(show_spinner = "Generating Prompt")
+def generate_prompt(ocr_extracted_text: str, json_structure: dict):
+    prompt = f"""Task: Text to JSON format
+
+Output: 
+
+1. Output
+2. Assign a Value
+3. Assign a Confidence 
+4. Assign a State: Extracted or Not Extracted
+5. Assign NA to unknown information
+
+JSON Template: 
+            
+{json.dumps(json_structure, indent=4, sort_keys=True)}
+
+Text:
+
+{ocr_extracted_text}
+"""
+    return prompt
+
+@st.cache_data(show_spinner = "LLM JSON Generation")
+def query_llm(prompt: str, model: str, email: str = "EMAIL", password: str = "PASSWORD"):
+    
+    sign = Login(st.secrets[email], st.secrets[password])
+    cookies = sign.login(save_cookies=True)
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict(),) 
+    llm_list = {llm.name:idx for idx, llm in enumerate(chatbot.get_available_llm_models())}
+    chatbot.switch_llm(llm_list[model])
+
+    llm_extracted_text = []
+    start_idx = 0
+    end_idx = 0
+    for idx, response in enumerate(chatbot.query(prompt,use_cache=True, truncate=10000, max_new_tokens=10000, return_full_text=True, stream=True)):
+        try:
+            llm_extracted_text.append(response["token"])
+            if ("{" in list(response["token"])) & (start_idx == 0):
+                start_idx = idx
+            if "}" in list(response["token"]):
+                end_idx = idx
+        except:
+            pass
+
+    llm_extracted_text = "".join(llm_extracted_text[start_idx:end_idx + 1])
+    print("Not regex")
+    print(llm_extracted_text)
+    return llm_extracted_text
+
+@st.cache_data(show_spinner=True)
+def create_dataframe(data):
+    flat_data = []
+    for key, value in data.items():
+        if key == 'articles':
+            for article in value:
+                for field, details in article.items():
+                    flat_data.append({
+                        'Field': field,
+                        'Value': details.get('value', ''),
+                        'Confidence': details.get('confidence', ''),
+                        'State': details.get('state', '')
+                    })
+        else:
+            if key == 'picture_quality':  # Special handling for picture_quality
+                flat_data.append({'Field': key, 'Value': value.get('value', ''), 'Confidence': '', 'State': ''})
+            elif isinstance(value, dict) and 'value' in value:
+                flat_data.append({
+                    'Field': key,
+                    'Value': value.get('value', ''),
+                    'Confidence': value.get('confidence', ''),
+                    'State': value.get('state', '')
+                })
+    return pd.DataFrame(flat_data)
+
+@st.cache_data(show_spinner=True)
+def update_json(data, df, edited_df):
+    for key, val in data.items():
+        if key == "articles":
+            for key2, val2 in enumerate(val):
+                for key3, val3 in val2.items():
+                    for key4, val4 in val3.items():
+                        try:
+                            data[key][key2][key3][key4] = edited_df.loc[df["Field"] == key3, key4.capitalize()].to_list()[0]
+                        except:
+                            pass
+        else:
+            for key2, val2 in val.items():
+                try:
+                    data[key][key2] = edited_df.loc[df["Field"] == key, key2.capitalize()].to_list()[0]
+                except:
+                    pass
+    return data
